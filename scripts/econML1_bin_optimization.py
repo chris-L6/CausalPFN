55cf7d8f5e087bd4101e1c4267c7a485d2bf5260
clstith <christopher@layer6.ai>
2025-11-11 14:10:19 -0500

    updating for econML Ex 1 dataset

---
+++ scripts/econML1_bin_optimization.py
@@ -0,0 +1,150 @@
+## Module imports
+import pandas as pd
+import sys
+sys.path.append("..")
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+from src.causalpfn import ATEEstimator
+from functools import reduce
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+## MAIN HYPERPARAMETER
+N_DISC_VALUES = [2]
+
+## Main body
+# Discretization function
+def discretize_treatment(T: np.ndarray, N: int) -> np.ndarray:
+    """Returns discretized version of T. Assumes range of T is [0, 1].
+
+    Args:
+        T (np.ndarray): The raw treatment data 
+        N (int): The number of discretized treatment values to use
+
+    Returns:
+        np.ndarray: The discretized treatment data
+    """
+    dt = 1 / (N - 1)
+    T_discrete = np.round(np.array(T, dtype=np.float32) / dt) * dt
+
+    return T_discrete
+
+# RMSE function
+def calculate_rmse(y_pred, y_true):
+    result = np.mean(np.power(y_pred - y_true, 2))
+    result = np.sqrt(result)
+
+    return result
+
+# # DGP from https://github.com/py-why/EconML/blob/main/notebooks/Causal%20Forest%20and%20Orthogonal%20Random%20Forest%20Examples.ipynb
+# # (Example 1)
+
+# DGP constants
+np.random.seed(123)
+def exp_te(x): return np.exp(2 * x[0])
+n = 1000
+n_w = 30
+support_size = 5
+n_x = 1
+# Outcome support
+support_Y = np.random.choice(range(n_w), size=support_size, replace=False)
+coefs_Y = np.random.uniform(0, 1, size=support_size)
+def epsilon_sample(n):
+    return np.random.uniform(-1, 1, size=n)
+# Treatment support
+support_T = support_Y
+coefs_T = np.random.uniform(0, 1, size=support_size)
+def eta_sample(n):
+    return np.random.uniform(-1, 1, size=n)
+
+# Generate controls, covariates, treatments and outcomes
+W = np.random.normal(0, 1, size=(n, n_w))
+X = np.random.uniform(0, 1, size=(n, n_x))
+# Heterogeneous treatment effects
+TE = np.array([exp_te(x_i) for x_i in X])
+T = np.dot(W[:, support_T], coefs_T) + eta_sample(n)
+# Rescale T to be in [0, 1]
+T_old = T
+T = (T - T_old.min()) / (T_old.max() - T_old.min())
+# Outcome
+Y = TE * T + np.dot(W[:, support_Y], coefs_Y) + epsilon_sample(n)
+
+# True effect: drf(t) = t * E[Theta(X)]
+def drf(t): return t * np.mean(TE)
+
+
+## Main prediction loop. Takes about 6 minutes to run.
+list_of_ates = [] # [(N_DISC, ates)]
+
+for N_DISC in N_DISC_VALUES:
+    print(f"N_DISC: {N_DISC}")
+    discrete_treatment_levels = np.linspace(0, 1, N_DISC)
+    T_discrete = discretize_treatment(T, N_DISC)
+    ates = []
+    for i, t in enumerate(discrete_treatment_levels[:-1]):
+        t0, t1 = discrete_treatment_levels[i], discrete_treatment_levels[i + 1]
+        ids = (np.abs(T_discrete - t0) < 1e-4) | (np.abs(T_discrete - t1) < 1e-4)
+        T_temp = np.where(np.abs(T_discrete[ids] - t0) < 1e-4, 0, 1).astype(np.float32)
+        X_temp = X[ids].astype(np.float32)
+        Y_temp = Y[ids].astype(np.float32)
+        causalpfn_ate = ATEEstimator(
+            device=device,
+            verbose=True
+        )
+        causalpfn_ate.fit(X_temp, T_temp, Y_temp)
+        ate = causalpfn_ate.estimate_ate()
+        ates.append(ate)
+    list_of_ates.append((N_DISC, ates))
+
+# To get a good RMSE estimate for all the predictions, need a large mesh
+fine_mesh = np.linspace(0, 1, 100) # fixed, fine mesh
+t_mesh = [np.linspace(0, 1, N_DISC) for N_DISC in N_DISC_VALUES] # to ensure all discrete treatment values are hit
+t_mesh += [fine_mesh]
+t_mesh = reduce(np.union1d, tuple(t_mesh))
+y_true = drf(t_mesh)
+
+# Get RMSE by evaluating on each of the t_mesh points
+rmse_dict = dict()
+for N_DISC, ates in list_of_ates:
+    epos = [sum(ates[:i]) for i in range(N_DISC)]
+    y_pred = np.interp(t_mesh, np.linspace(0, 1, N_DISC), epos)
+    error = calculate_rmse(y_pred, y_true)
+    rmse_dict[N_DISC] = np.round(error, 4)
+
+# Create dataframe, print optimizer, and save the data
+df = pd.DataFrame.from_dict(rmse_dict, orient="index", columns=["RMSE"])
+row = df[df["RMSE"] == df["RMSE"].min()]
+df.to_csv("../output/EconML1_rmse_table.csv")
+
+fig = plt.figure()
+plt.bar(df.index, height=df["RMSE"])
+plt.xlabel("N_DISC")
+plt.ylabel("RMSE")
+plt.title("RMSE of different choices of N_DISC")
+plt.savefig("../output/EconML1_rmse_bar_chart.png")
+
+# Plot predictions
+fig = plt.figure(figsize=(15, 10))
+i = 0 # counter for visual effects
+for N_DISC, ates in list_of_ates:
+    epos = [sum(ates[:i]) for i in range(N_DISC)]
+    if df.loc[N_DISC, "RMSE"] == df.min().values: # Emphasize best prediction
+        plt.plot(np.linspace(0.0, 1.0, N_DISC), epos, '-', linewidth=2,
+                 label=f"N_DISC={N_DISC} (Best)", c="blue", zorder=14)
+    else:
+        plt.plot(np.linspace(0.0, 1.0, N_DISC), epos, '--', linewidth=1,
+                 label=f"N_DISC={N_DISC}")
+    i += 1
+
+# True dose-response curve
+plt.plot(np.linspace(0.0, 1.0, len(y_true)), y_true,
+         label="True dose-response curve", linewidth=2, c='k', zorder=15)
+
+plt.title("$E[Y_t]$ (CausalPFN estimate using ATEs) on EconML Synthetic data") # Note E[Y_t] = E[Y | do(T = t)]
+plt.xlabel("Treatment dosage t")
+plt.ylabel("Expected outcome Y")
+
+plt.legend(fontsize=8)
+plt.savefig("../output/EconML1_drf_plot.png", dpi=500)
