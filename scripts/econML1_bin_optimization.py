ceb3cfde78ee024a42cd6e01fdffaffa778a9a66
chris-L6 <christopher@layer6.ai>
2025-11-11 15:28:57 -0500

    EconML ex 1, large N values

--- scripts/econML1_bin_optimization.py
+++ scripts/econML1_bin_optimization.py
@@ -12,7 +12,7 @@ from functools import reduce
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 ## MAIN HYPERPARAMETER
-N_DISC_VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+N_DISC_VALUES = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
 
 ## Main body
 # Discretization function
