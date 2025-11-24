c5c4d34c08aec7dada10209717fcafd6ef71dc30
clstith <christopher@layer6.ai>
2025-11-24 16:10:48 -0500

    making more CI scripts for different datasets

---
+++ my-work/Week_11-24/scripts/inference_econMLEx1-quantile-all.py
@@ -0,0 +1,109 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+import datetime
+import sys
+sys.path.append("../discretized-causalpfn")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+from discretize import DataDiscretizer
+from inference import DiscreteCausalPFN
+from scipy.interpolate import interp1d
+
+## HELPER FUNCTIONS AND PARAMETERS
+# RMSE function
+def calculate_rmse(y_pred, y_true):
+    result = np.mean(np.power(y_pred - y_true, 2))
+    result = np.sqrt(result)
+
+    return result
+
+fine_t_mesh = np.linspace(0, 1, 100)
+
+# Parameters
+scheme = "quantile"
+comparison_method = "all"
+N_DISC_VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+
+## DGP 
+# DGP from: https://github.com/py-why/EconML/blob/main/notebooks/Causal%20Forest%20and%20Orthogonal%20Random%20Forest%20Examples.ipynb
+data_name = "econ-ML-ex1"
+np.random.seed(123)
+def exp_te(x): return np.exp(2 * x[0])
+n = 2000
+n_w = 30
+support_size = 5
+n_x = 1
+# Outcome support
+support_Y = np.random.choice(range(n_w), size=support_size, replace=False)
+coefs_Y = np.random.uniform(0, 1, size=support_size)
+def epsilon_sample(n):
+    return np.random.uniform(-1, 1, size=n)
+# Treatment support
+support_T = support_Y
+coefs_T = np.random.uniform(0, 1, size=support_size)
+def eta_sample(n):
+    return np.random.uniform(-1, 1, size=n)
+# Generate controls, covariates, treatments and outcomes
+W = np.random.normal(0, 1, size=(n, n_w))
+X = np.random.uniform(0, 1, size=(n, n_x))
+# Heterogeneous treatment effects
+TE = np.array([exp_te(x_i) for x_i in X])
+T = np.dot(W[:, support_T], coefs_T) + eta_sample(n)
+# Rescale T to be in [0, 1]
+T_old = T
+T = (T - T_old.min()) / (T_old.max() - T_old.min())
+# Outcome
+Y = TE * T + np.dot(W[:, support_Y], coefs_Y) + epsilon_sample(n)
+# True effect: drf(t) = t * E[Theta(X)]
+def drf(t): return t * np.mean(TE)
+
+## INFERENCE
+epos_collection = dict() # collect all results across all N_DISC in N_DISC_VALUES
+CI_collection = dict() 
+for N_DISC in N_DISC_VALUES:
+    print(f"N_DISC: {N_DISC}")
+    discretizer = DataDiscretizer(scheme=scheme)
+    T_discrete, T_vals = discretizer.discretize_treatment(T, N_DISC)
+    model = DiscreteCausalPFN(
+        comparison_method=comparison_method,
+        N_DISC=N_DISC,
+        device=device,
+        verbose=True
+    )
+    epos_dict, CI_dict = model.predict_epos(
+        X,
+        T_discrete, 
+        Y, 
+        discrete_treatment_vals=T_vals,
+        take_mean=True, 
+        alpha=0.5
+    )
+    epos_collection[N_DISC] = epos_dict
+    CI_collection[N_DISC] = CI_dict
+
+## CREATING AND EXPORTING DATA
+multi_indices = pd.MultiIndex.from_tuples(
+    [(N, t) for N in epos_collection for t in epos_collection[N]],
+    names=["N_DISC", "T"]
+)
+cols = ["estimated_epo", "CI_lower_bound", "CI_upper_bound"]
+epo_df = pd.DataFrame(index=multi_indices, 
+                      columns=cols)
+for N in epos_collection:
+    for t in epos_collection[N]:
+        epo_df.loc[(N, t), "estimated_epo"] = epos_collection[N][t]
+        epo_df.loc[(N, t), "CI_lower_bound"] = CI_collection[N][t][0].item()
+        epo_df.loc[(N, t), "CI_upper_bound"] = CI_collection[N][t][1].item()
+
+# For filenaming so as not to overwrite previous output 
+month = datetime.datetime.now().month
+date = datetime.datetime.now().day
+hour = datetime.datetime.now().hour
+minute = datetime.datetime.now().minute
+date_string = f"{month}-{date}_{hour}h{minute}m"
+# Save the DataFrame
+file_name = f"EPO_df_with_CI_{data_name}_{scheme}_{comparison_method}_{date_string}"
+file_location = "../output"
+epo_df.to_csv(f"{file_location}/{file_name}.csv")
