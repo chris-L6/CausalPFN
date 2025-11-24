142c93e45bae8c0258e9be8689ef045e838327c9
clstith <christopher@layer6.ai>
2025-11-24 16:16:10 -0500

    uniform CI scripts ready

---
+++ my-work/Week_11-24/scripts/inference_ADMIT-synthetic_uniform_all.py
@@ -0,0 +1,105 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+import datetime
+import sys
+sys.path.append("../discretized-causalpfn")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+from discretize import DataDiscretizer
+from inference import DiscreteCausalPFN
+from scipy.interpolate import interp1d
+
+## HELPER FUNCTIONS AND PARAMETERS
+# RMSE function
+def calculate_rmse(y_pred, y_true):
+    result = np.mean(np.power(y_pred - y_true, 2))
+    result = np.sqrt(result)
+
+    return result
+
+fine_t_mesh = np.linspace(0, 1, 100)
+
+# Parameters
+scheme = "uniform"
+comparison_method = "all"
+N_DISC_VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+
+## DGP 
+data_name = "ADMIT-synthetic"
+# DGP from https://github.com/CausalTeam/ADMIT/blob/main
+def sigmoid(t): return 1. / (1. + np.exp(-1. * t))
+n, d = 2000, 6
+X = np.random.uniform(-1, 1, (n, d))
+x1 = X[:, 0]
+x2 = X[:, 1]
+x3 = X[:, 2]
+x4 = X[:, 3]
+x5 = X[:, 4]
+x6 = X[:, 5]
+# Generate treatment 
+mu_1 =  (10. * np.sin(np.maximum(x1, np.maximum(x2, x3))) + np.maximum(x3, np.maximum(x4, x5))**3)/(1. + (x1 + x5)**2) + np.sin(0.5 * x3) * (1. + np.exp(x4 - 0.5 * x3)) + x3**2 + 2. * np.sin(x4) + 2.*x5 - 6.5
+T = np.random.normal(mu_1, 0.5)
+T = sigmoid(T)
+# Generate outcomes
+mu_2 = np.cos(2 * np.pi * (T - 0.5)) * (T ** 2 + (4 * np.maximum(x1, x6) ** 3) * np.sin(x4) / (1 + 2 * x3 ** 2))
+Y = np.random.normal(mu_2, 0.5)
+# Generated expected potential outcomes empirically
+treatment_doses = np.linspace(0, 1, 100)
+effects = []
+for t in treatment_doses:
+    T_intervention = t * np.ones(T.shape)
+    mu_2_intervention = np.cos(2 * np.pi * (T_intervention - 0.5)) * (T_intervention ** 2 + (4 * np.maximum(x1, x6) ** 3) * np.sin(x4) / (1 + 2 * x3 ** 2))
+    effects.append(mu_2_intervention.mean())
+effects = np.array(effects)
+drf = interp1d(treatment_doses, effects)
+
+## INFERENCE
+epos_collection = dict() # collect all results across all N_DISC in N_DISC_VALUES
+CI_collection = dict() 
+for N_DISC in N_DISC_VALUES:
+    print(f"N_DISC: {N_DISC}")
+    discretizer = DataDiscretizer(scheme=scheme)
+    T_discrete, T_vals = discretizer.discretize_treatment(T, N_DISC)
+    model = DiscreteCausalPFN(
+        comparison_method=comparison_method,
+        N_DISC=N_DISC,
+        device=device,
+        verbose=True
+    )
+    epos_dict, CI_dict = model.predict_epos(
+        X,
+        T_discrete, 
+        Y, 
+        discrete_treatment_vals=T_vals,
+        take_mean=True, 
+        alpha=0.5
+    )
+    epos_collection[N_DISC] = epos_dict
+    CI_collection[N_DISC] = CI_dict
+
+## CREATING AND EXPORTING DATA
+multi_indices = pd.MultiIndex.from_tuples(
+    [(N, t) for N in epos_collection for t in epos_collection[N]],
+    names=["N_DISC", "T"]
+)
+cols = ["estimated_epo", "CI_lower_bound", "CI_upper_bound"]
+epo_df = pd.DataFrame(index=multi_indices, 
+                      columns=cols)
+for N in epos_collection:
+    for t in epos_collection[N]:
+        epo_df.loc[(N, t), "estimated_epo"] = epos_collection[N][t]
+        epo_df.loc[(N, t), "CI_lower_bound"] = CI_collection[N][t][0].item()
+        epo_df.loc[(N, t), "CI_upper_bound"] = CI_collection[N][t][1].item()
+
+# For filenaming so as not to overwrite previous output 
+month = datetime.datetime.now().month
+date = datetime.datetime.now().day
+hour = datetime.datetime.now().hour
+minute = datetime.datetime.now().minute
+date_string = f"{month}-{date}_{hour}h{minute}m"
+# Save the DataFrame
+file_name = f"EPO_df_with_CI_{data_name}_{scheme}_{comparison_method}_{date_string}"
+file_location = "../output"
+epo_df.to_csv(f"{file_location}/{file_name}.csv")
---
+++ my-work/Week_11-24/scripts/inference_vahid-linear_uniform_all.py
@@ -0,0 +1,88 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+import datetime
+import sys
+sys.path.append("../discretized-causalpfn")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+from discretize import DataDiscretizer
+from inference import DiscreteCausalPFN
+from scipy.interpolate import interp1d
+
+## HELPER FUNCTIONS AND PARAMETERS
+# RMSE function
+def calculate_rmse(y_pred, y_true):
+    result = np.mean(np.power(y_pred - y_true, 2))
+    result = np.sqrt(result)
+
+    return result
+
+fine_t_mesh = np.linspace(0, 1, 100)
+
+# Parameters
+scheme = "uniform"
+comparison_method = "all"
+N_DISC_VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+
+
+## DGP 
+data_name = "vahid-linear"
+np.random.seed(42)
+n, d = 2000, 3
+X = np.random.normal(1, 1, size=(n, d)).astype(np.float32)
+T = (X[:, 0] - X[:, 1] + 2 * X[:, 2] + 2 + np.random.normal(0, 3, size=n)).astype(np.float32)
+T = T - T.min() # Rescale
+T = T / T.max() # Rescale
+Y = (3 * X[:, 0] + X[:, 1] - 0.5 * X[:, 2] + 3 * T + np.random.normal(0, 2, size=n)).astype(np.float32)
+def drf(t): return 3.5 + 3 * t # true dose-response function
+
+## INFERENCE
+epos_collection = dict() # collect all results across all N_DISC in N_DISC_VALUES
+CI_collection = dict() 
+for N_DISC in N_DISC_VALUES:
+    print(f"N_DISC: {N_DISC}")
+    discretizer = DataDiscretizer(scheme=scheme)
+    T_discrete, T_vals = discretizer.discretize_treatment(T, N_DISC)
+    model = DiscreteCausalPFN(
+        comparison_method=comparison_method,
+        N_DISC=N_DISC,
+        device=device,
+        verbose=True
+    )
+    epos_dict, CI_dict = model.predict_epos(
+        X,
+        T_discrete, 
+        Y, 
+        discrete_treatment_vals=T_vals,
+        take_mean=True, 
+        alpha=0.5
+    )
+    epos_collection[N_DISC] = epos_dict
+    CI_collection[N_DISC] = CI_dict
+
+## CREATING AND EXPORTING DATA
+multi_indices = pd.MultiIndex.from_tuples(
+    [(N, t) for N in epos_collection for t in epos_collection[N]],
+    names=["N_DISC", "T"]
+)
+cols = ["estimated_epo", "CI_lower_bound", "CI_upper_bound"]
+epo_df = pd.DataFrame(index=multi_indices, 
+                      columns=cols)
+for N in epos_collection:
+    for t in epos_collection[N]:
+        epo_df.loc[(N, t), "estimated_epo"] = epos_collection[N][t]
+        epo_df.loc[(N, t), "CI_lower_bound"] = CI_collection[N][t][0].item()
+        epo_df.loc[(N, t), "CI_upper_bound"] = CI_collection[N][t][1].item()
+
+# For filenaming so as not to overwrite previous output 
+month = datetime.datetime.now().month
+date = datetime.datetime.now().day
+hour = datetime.datetime.now().hour
+minute = datetime.datetime.now().minute
+date_string = f"{month}-{date}_{hour}h{minute}m"
+# Save the DataFrame
+file_name = f"EPO_df_with_CI_{data_name}_{scheme}_{comparison_method}_{date_string}"
+file_location = "../output"
+epo_df.to_csv(f"{file_location}/{file_name}.csv")
